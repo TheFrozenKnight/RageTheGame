d405bdeac69bf98fdfed1572c419b8bedbc09b2c
TheFrozenKnight <64625343+TheFrozenKnight@users.noreply.github.com>
2021-10-22 21:02:41 +0530

    update

---
+++ Assets/Scripts/UI/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class SplashScreen : MonoBehaviour
+{
+    VideoPlayer video;
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+    void CheckOver(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
+
