48e4412128044c1ee820b09faf801666543bd5d3
Prashant9212 <pratham210401@gmail.com>
2021-10-23 22:45:35 +0530

    Portal

---
+++ Assets/Scripts/UI/DisableOnExit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnExit : StateMachineBehaviour
+{
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.SetActive(false);
+    }
+}
